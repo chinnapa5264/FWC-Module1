c23bdd41c4ce5005326ef36089a45bb5ca869bdf
Chinnapareddy Alavala <108186801+chinnapa5264@users.noreply.github.com>
2022-08-25 13:24:36 +0530

    Add files via upload

--- Assignment-1 using LED/codes/src/main.cpp.cpp
+++ Assignment-1 using LED/codes/src/main.cpp.cpp
@@ -1,5 +1,5 @@
 #include<Arduino.h>
-int A=0,B=0,C=1;
+int A=0,B=0,C=0;
 int X,Y;
 
 //Code released under GNU GPL.  Free to use for anything.
@@ -9,12 +9,18 @@ void verify(int F)
 }
 // the setup function runs once when you press reset or power the board
 void setup() {
-    pinMode(13, OUTPUT);  
+    pinMode(13, OUTPUT); 
+    pinMode(2, INPUT);
+    pinMode(3, INPUT);
+    pinMode(4, INPUT); 
 }
 
 // the loop function runs over and over again forever
 void loop ()
 {
+C = digitalRead(4);//LSB
+B = digitalRead(5);
+A = digitalRead(6);;//MSB
 X= A || C;
 Y= A || (!A && C) || (B && C);
 if(X=Y) 
